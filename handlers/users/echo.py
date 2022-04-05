31d5a773d9b0d10381345e4dc01c8565a97b0b64
vol4ok <vol4oks.d@yandex.ru>
2022-04-05 21:44:03 +0300

    запуск

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,6 +1,15 @@
 from aiogram import types
 from loader import dp
+from data.config import admins
+from aiogram.types import message
+from .interf import admin_interf, users_inerf
 
+@dp.message_handler(text='/info')
+async def show_info(message: types.Message):
+    if message.from_user.id in admins:
+        await admin_interf.info(message)
+    else:
+        await users_inerf.info(message)
 
 @dp.message_handler()
 async def bot_echo(message: types.Message):
---
+++ handlers/users/interf/admin_interf.py
@@ -0,0 +1,7 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+from aiogram.types import message
+from loader import dp
+
+async def info(message: types.Message):
+    await message.answer("Бот для отпраки моделей на печать, Вы Админ")
